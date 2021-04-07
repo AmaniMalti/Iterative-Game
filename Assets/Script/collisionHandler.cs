baa64281a388aecadabc72495e66201528aad31d
AmaniMalti <amanimalti.95@gmail.com>
2021-04-06 23:48:58 -0400

    Initial commit

---
+++ Assets/Script/collisionHandler.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class collisionHandler : MonoBehaviour
+    
+{
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip crash;
+
+    AudioSource audioSource;
+
+    bool isTransitioning = false;
+    bool collisionDisabled = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>(); 
+    }
+
+    void Update()
+    {
+        RespondToDebugKeys();
+    }
+
+    void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionDisabled = !collisionDisabled;  // toggle collision
+        }
+    }
+
+        void OnCollisionEnter(Collision other)
+    {
+        if (isTransitioning || collisionDisabled) { return; }
+
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartCrashSequence();
+                break;
+        }
+    }
+
+    void StartSuccessSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        GetComponent<movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+
+    }
+
+    void StartCrashSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(crash);
+        GetComponent<movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+}
+// credit: https://www.udemy.com/course/unitycourse2/learn/lecture/24879736?start=0#overview
